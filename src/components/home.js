0e5b2d13a5082d9c96cf9c3f44a854f1d51a0421
mono motapa <bahobab@hotmail.com>
2020-10-28 21:22:38 -0500

    initial commit: Setup app skeleton. Connected to Strapi api

--- src/components/home.js
+++ src/components/home.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import {Container, } from 'semantic-ui-react';
+import {Container, Grid} from 'semantic-ui-react';
 
 import Hero from '../components/hero'
+// import Events from '../components/events';
+// import Artists from '../components/artists';
+// import Gallery from '../components/'
 
 function Home() {
   return (
-    <>
+    <Grid>
       <Hero />
       <h2 id='info'>Who we are</h2>
       <h2 id='events'>Events Section</h2>
       <h2 id='artists'>Artists Section</h2>
       <h2 id='gallery'>Gallery Section</h2>
-    </>
+    </Grid>
     )
 }
 
