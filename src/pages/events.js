a002685da43a4402e55442729c10364899a52675
mono motapa <bahobab@hotmail.com>
2020-11-11 18:02:36 -0600

    fixing conflict about readme

--- src/pages/events.js
+++ src/pages/events.js
@@ -2,10 +2,11 @@ import React, { Suspense } from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import { Header, Segment } from 'semantic-ui-react';
 
+import Events from '../events';
 import Layout from '../components/layout';
 import Seo from '../components/seo';
 
-const Events = React.lazy(() => import('../components/events'));
+// const Events = React.lazy(() => import('../components/events'));
 
 export const query = graphql`
   {
@@ -49,9 +50,9 @@ function EventsPage() {
             borderRadius: '3px',
           }}
         />
-        <Suspense fallback={<div>...loading</div>}>
-          <Events events={data.events.nodes} />
-        </Suspense>
+        {/* <Suspense fallback={<div>...loading</div>}> */}
+        <Events events={data.events.nodes} />
+        {/* </Suspense> */}
       </Segment>
     </Layout>
   );
